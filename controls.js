64d9489ae5db13172658147a55daec248ec3bffb
aeolus-1 <121691@student.hillbrook.qld.edu.au>
2023-02-21 13:36:25 +1000

    the yay

--- controls.js
+++ controls.js
@@ -10,8 +10,8 @@ document.addEventListener("mousedown", (e)=>{
     mouse.pos.y = e.offsetY
 
     var screenPos = v(
-        (mouse.pos.x+camera.pos.x)/camera.zoom,
-        (mouse.pos.y+camera.pos.y)/camera.zoom
+        (mouse.pos.x-camera.pos.x)*camera.zoom,
+        (mouse.pos.y-camera.pos.y)*camera.zoom
     )
     runClick(v(
         Math.floor(screenPos.x/camera.gridScale),
@@ -19,3 +19,13 @@ document.addEventListener("mousedown", (e)=>{
     ),e.which==3)
     
 })
+var keys = {}
+document.addEventListener("keydown", (e)=>{keys[e.key.toLowerCase()]=true})
+document.addEventListener("keyup", (e)=>{keys[e.key.toLowerCase()]=false})
+
+function runControls() {
+    if (keys.w) camera.target.y -= -5
+    if (keys.s) camera.target.y += -5
+    if (keys.a) camera.target.x -= -5
+    if (keys.d) camera.target.x += -5
+}
