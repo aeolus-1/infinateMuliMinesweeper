631687bb7e5f581416123883369752fc029fe4b8
reefreef <benreed2018@outlook.com>
2024-05-21 12:05:11 +1000

    how did i fumble this hard, never again

--- controls.js
+++ controls.js
@@ -86,10 +86,10 @@ document.addEventListener("keyup", (e)=>{keys[e.key.toLowerCase()]=false})
 
 function runControls() {
     var speed = 20*camera.zoom
-    if (keys.w||key.ArrowUp) camera.target.y -= -speed
-    if (keys.s||key.ArrowDown) camera.target.y += -speed
-    if (keys.a||key.ArrowLeft) camera.target.x -= -speed
-    if (keys.d||key.ArrowRight) camera.target.x += -speed
+    if (keys.w||keys.ArrowUp) camera.target.y -= -speed
+    if (keys.s||keys.ArrowDown) camera.target.y += -speed
+    if (keys.a||keys.ArrowLeft) camera.target.x -= -speed
+    if (keys.d||keys.ArrowRight) camera.target.x += -speed
 
     function add(num) {camera.targetZoom = clamp(camera.targetZoom+num,0.25, 2)}
 
