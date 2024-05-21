d4f1169d40b65893c07e3ea30dc0cf1c3b39d42c
reefreef <benreed2018@outlook.com>
2024-05-21 11:15:59 +1000

    added frontend chat - this is so fucked

---
+++ chat.js
@@ -0,0 +1,44 @@
+function appendMsgToChat(msg, user) {
+    chatContainer = document.getElementById("chatContainer")
+    let msgBox = createElementFromHTML(`<div>
+    <span>[error]</span>
+    <span>error, dm reef if you see this</span>
+  </div>`)
+
+
+  // INSERTS IT AS TEXT ONLY!!!!!!!!!!!!!!!!!!!!
+  // NO MORE SCRIPT INJECTING!!!!!!!!!!!!!!!!!!!
+  msgBox.children[0].textContent = `[${user}]`
+  msgBox.children[1].textContent = `[${msg}]`
+
+  chatContainer.appendChild(msgBox)
+
+}
+
+
+function submitMsg(msg,user) {
+    console.log("emiting new msg - ", `[${user}] ${msg}`)
+    
+    // idfk
+    
+    
+    return 0
+}
+
+chatSubmit = document.getElementById("chatSubmit")
+chatSubmitButton = document.getElementById("chatSubmitButton")
+
+
+
+chatSubmitButton.onclick = ()=>{
+    let currentUsername = document.getElementById("chatUsername").value,
+        msg = chatSubmit.value
+
+    document.getElementById("chatSubmit").value = ""
+    submitMsg(msg, currentUsername)
+}
+chatSubmit.addEventListener("keydown",(e)=>{
+    if (e.key=="Enter"){
+        chatSubmitButton.click()
+    }
+})
